62eebfd2cdf9aada2cf3fdcc032af08c99dd7a86
Stephenna J Corry <Sjcstephenna@gmail.com>
2021-08-24 22:53:33 -0500

    Hopefully fixed errors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-
+console.log(__dirname)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
